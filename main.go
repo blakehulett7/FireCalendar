db57b4cae51af2b53dd08fb7515d46ad138103e7
blakehulett7 <blake.hulett7@gmail.com>
2024-08-13 15:14:03 -0500

    Starting the menu, gonna be fun to hand roll this!

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	fmt.Println("Christ is King!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		clear := exec.Command("clear")
+		clear.Stdout = os.Stdout
+		clear.Run()
+		fmt.Println("Welcome to the FireCalendar, Christ is King!")
+		fmt.Println("")
+		DrawMenu()
+		scanner.Scan()
+	}
 }
+
+const cyan = "\033[36m"
+const reset = "\033[0m"
--- menu.go
+++ menu.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-func StartMenu() {
+func DrawMenu() {
+	currentIndex := 0
+	options := getMenuOptions()
+	optionsArray := []string{}
+	for option := range options {
+		optionsArray = append(optionsArray, option)
+	}
+	for index, option := range optionsArray {
+		if index == currentIndex {
+			fmt.Println(cyan + "> " + option + reset)
+			continue
+		}
+		fmt.Println(" ", option)
+	}
 }
 
 func getMenuOptions() map[string]func() {
 	return map[string]func(){
 		"Add Event":            AddEvent,
 		"View Upcoming Events": ViewEvents,
+		"Exit":                 Exit,
 	}
 }
 
@@ -20,4 +35,8 @@ func AddEvent() {
 
 func ViewEvents() {
 	fmt.Println("Viewing events...")
+}
+
+func Exit() {
+	os.Exit(0)
 }
