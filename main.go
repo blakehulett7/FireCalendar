0a8835a96754de12ff1eabbda246da708487f3df
blakehulett7 <blake.hulett7@gmail.com>
2024-08-13 14:48:40 -0500

    Adding a basic menu

--- main.go
+++ main.go
@@ -7,3 +7,21 @@ import (
 func main() {
 	fmt.Println("Christ is King!")
 }
+
+func StartMenu() {
+}
+
+func getMenuOptions() map[string]func() {
+	return map[string]func(){
+		"Add Event":            AddEvent,
+		"View Upcoming Events": ViewEvents,
+	}
+}
+
+func AddEvent() {
+	fmt.Println("Adding event...")
+}
+
+func ViewEvents() {
+	fmt.Println("Viewing events...")
+}
